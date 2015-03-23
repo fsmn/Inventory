01aaf333db740a914392e0d1cafbbba2dbe963cf
Chris Dart <technology@fsmn.org>
2015-03-23 16:50:46 -0500

    more asset editing/adding features

--- application/views/asset/edit.php
+++ application/views/asset/edit.php
@@ -3,67 +3,67 @@
 ?>
 <form id="asset_editor" name="asset_editor"
 	action="<? echo site_url("asset/$action"); ?>" method="post">
-	<input type="hidden" name="kAsset" id="kAsset"
-		value="<?=getValue($asset, 'kAsset')?>" /> <input type="hidden"
+	<input type="hidden" name="id" id="id"
+		value="<?=get_value($asset, 'id')?>" /> <input type="hidden"
 		name="action" id="action" value="<?=$action?>" />
 	<p>
 		<label for="product">Product Name&nbsp;</label> <input type="text"
-			id="product" name="product" value="<?=getValue($asset, 'product');?>" />
+			id="product" name="product" value="<?=get_value($asset, 'product');?>" />
 	</p>
 	<p>
 		<label for="name">Asset Name</label> <input type="text" id="name"
-			name="name" value="<?=getValue($asset, 'name');?>" />
+			name="name" value="<?=get_value($asset, 'name');?>" />
 	</p>
 
 	<p>
 		<label for="version">Version&nbsp;</label> <input type="text"
-			id="version" name="version" value="<?=getValue($asset, 'version');?>" />
+			id="version" name="version" value="<?=get_value($asset, 'version');?>" />
 	</p>
 
 	<p>
-		<label for="type">Type&nbsp;</label> <span id='typeField'> <?=form_dropdown('type', $typePairs, getValue($asset, 'type'), 'id="type"');?>
+		<label for="type">Type&nbsp;</label> <span id='type_field'> <?=form_dropdown('type', $types, get_value($asset, 'type'), 'id="type"');?>
 		</span>
 	</p>
 	<p>
 		<label for="serial_number">Serial Number&nbsp;</label> <input
 			type="text" name="serial_number"
-			value="<?=getValue($asset,'serial_number');?>"
-			id="serial_number-<?=getValue($asset,"kAsset","0");?>"
+			value="<?=get_value($asset,'serial_number');?>"
+			id="serial_number-<?=get_value($asset,"id","0");?>"
 			class="serial_number" />
 	</p>
 	<p>
 		<label for="year_acquired">Year Acquired</label> <span
 			id="yearAquiredField"> <input type="text" id="year_acquired"
-			name="year_acquired" value="<?=getValue($asset,"year_acquired");?>"
+			name="year_acquired" value="<?=get_value($asset,"year_acquired");?>"
 			size="5" />
 		</span>
 	</p>
 	<p>
-		<label for="source">Source</label> <span id="sourceField"> <input
+		<label for="source">Source</label> <span id="source"> <input
 			type="text" id="source" name="source"
-			value="<?=getValue($asset,"source");?>" />
+			value="<?=get_value($asset,"source");?>" />
 		</span>
 	</p>
 	<p>
-		<label for="status">Status&nbsp;</label> <span id='statusField'> <?=form_dropdown('status', $statusPairs, getValue($asset, 'status'), 'id="status"');?>
+		<label for="status">Status&nbsp;</label> <span id='status'> <?=form_dropdown('status', $statuses, get_value($asset, 'status'), 'id="status"');?>
 		</span>
 	</p>
 	<div id="year_removed_block"
-	<? if(getValue($asset, 'status') != "Deacquisitioned" && getValue($asset, 'status') != "Destroyed" && getValue($asset, 'status') != "Stolen"){echo "style='display:none'";}?>>
+	<? if(get_value($asset, 'status') != "Deacquisitioned" && get_value($asset, 'status') != "Destroyed" && get_value($asset, 'status') != "Stolen"){echo "style='display:none'";}?>>
 		<label for="year_removed">Year Removed</label> <input type="text"
 			id="year_removed" name="year_removed"
-			value="<?=getValue($asset,"year_removed");?>" size="5" />
+			value="<?=get_value($asset,"year_removed");?>" size="5" />
 	</div>
 	<p>
-		<label for="kDeveloper">Developer&nbsp;</label> <span
-			id='developerField'> <?=form_dropdown('kDeveloper', $developerPairs, getValue($asset, 'kDeveloper'), 'id="kDeveloper"')?>
+		<label for="vendor_id">Developer&nbsp;</label> <span
+			id='vendor_list'> <?=form_dropdown('vendor_id', $developers, get_value($asset, 'vendor_id'), 'id="vendor_id"')?>
 		</span>
 	</p>
 	<p>
 	<label for="po">Purchase Order</label>
-	<span id="poField"><input
+	<span id="po-field"><input
 			type="text" id="po" name="po" size="5"
-			value="<?=getValue($asset,"po");?>" /></span>
+			value="<?=get_value($asset,"po");?>" /></span>
 	</p>
 	<p>
 		<? if($action == "update"){ ?>
