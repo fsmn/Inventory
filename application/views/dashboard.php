5dcd83e053a0a56202a807a116068b476fcfef81
Chris Dart <technology@fsmn.org>
2015-11-24 17:10:17 -0600

    minor adjustments for iPhone 4 and other smaller screen devices.

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,6 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+$buttons [] = array(
+		"text"=>"Timesheet <i class='fa fa-clock-o'></i>",
+		"href"=>site_url("timesheet"),
+		"style"=>"default",
+);
 $buttons[] = array("text"=>"New PO","href"=>site_url("po/create"), "style"=>"new","class"=>"create-po create dialog");
 if($this->ion_auth->in_group(1)){
 $buttons[] = array("text"=>"New Asset","href"=>site_url("asset/create"), "style"=>"new","class"=>"create-asset create dialog");
@@ -8,5 +12,3 @@ $buttons[] = array("text"=>"New Asset","href"=>site_url("asset/create"), "style"
 ?>
 
 <? echo create_button_bar($buttons,"toolbar");?>
-<p>
-This is will be a dashboard where you can select tasks or look at statistics.</p>
