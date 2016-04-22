1fc4dece013351a6b8e4d05d6d35317d3791eb8d
chrisdart <chrisd@fsmn.org>
2016-04-22 17:21:19 -0500

    bug fix for approval

--- application/views/po/email/approval_request.php
+++ application/views/po/email/approval_request.php
@@ -1,4 +1,4 @@
 <?php
 ?>
-<h2><?php printf("%s %s",$po->first_name, $po->last_name); ?> has requested your approval for a purchase from <?php echo $po->vendor;?></h2>
+<h2><?php printf("%s %s",$po->first_name, $po->last_name); ?> has requested your approval for a purchase from <?php echo $po->vendor->name;?></h2>
 <p>Please click on the following link to review and approve this purchase order: <?php echo site_url("po/view/$po->po?approval_request=TRUE");?></p>
